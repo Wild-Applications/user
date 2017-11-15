2d8ebb7005da2a12912ac437b0612650fbec8b94
mewild <michael@wildapplications.com>
2017-11-15 16:02:35 +0000

    Password recovery

--- src/app.js
+++ src/app.js
@@ -210,7 +210,7 @@ server.post("recover/:guid", function(req, res, next){
   if(req.params.guid && req.body.password){
     accountClient.resetPassword({guid: req.params.guid, password:req.body.password}, function(err, response){
       if(err){
-        err = JSON.stringify(err);
+        err = JSON.stringify(err.error);
         console.log(err);
         res.status = err.error.status;
         res.send(err);
